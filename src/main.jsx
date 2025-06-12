853a1eb625bf5439baa9116b0c7d4a5de07f90b1
kal-aa <sadkalshayee@gmail.com>
2025-06-13 02:06:09 +0300

    update the main.jsx to user browserRouter and change the production url to / for vercel

--- src/main.jsx
+++ src/main.jsx
@@ -2,17 +2,19 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.jsx";
-import { HashRouter } from "react-router-dom";
+import { BrowserRouter } from "react-router-dom";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 
 const queryClient = new QueryClient();
 
 createRoot(document.getElementById("root")).render(
-  <HashRouter basename={import.meta.env.VITE_PUBLIC_URL}>
+  // <HashRouter basename={import.meta.env.VITE_PUBLIC_URL}>
+  <BrowserRouter>
     <StrictMode>
       <QueryClientProvider client={queryClient}>
         <App />
       </QueryClientProvider>
     </StrictMode>
-  </HashRouter>
+  </BrowserRouter>
+  // </HashRouter>
 );
