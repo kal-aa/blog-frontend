70f7d736a309537b9b30e8e681e5cf68020c8491
kal-aa <sadkalshayee@gmail.com>
2025-06-12 18:41:13 +0300

    use hashrouter

--- src/main.jsx
+++ src/main.jsx
@@ -2,17 +2,17 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.jsx";
-import { BrowserRouter } from "react-router-dom";
+import { HashRouter } from "react-router-dom";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 
 const queryClient = new QueryClient();
 
 createRoot(document.getElementById("root")).render(
-  <BrowserRouter basename={import.meta.env.VITE_PUBLIC_URL}>
+  <HashRouter basename={import.meta.env.VITE_PUBLIC_URL}>
     <StrictMode>
       <QueryClientProvider client={queryClient}>
         <App />
       </QueryClientProvider>
     </StrictMode>
-  </BrowserRouter>
+  </HashRouter>
 );
